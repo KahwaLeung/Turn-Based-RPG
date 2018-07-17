189625a9923d5a2a5dbed33f07909e10918d8cc9
KahwaLeung <632953630@qq.com>
2018-07-17 16:28:54 +0800

    1st version

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    [HideInInspector]
+    public GameObject followingObject;
+
+    private Vector3 offset;
+
+    private void Awake()
+    {
+        followingObject = GameObject.FindWithTag("Player");
+    }
+
+    private void Start()
+    {
+        offset = followingObject.transform.position - transform.position;
+    }
+
+    private void Update()
+    {
+        this.transform.position = followingObject.transform.position - offset;
+    }
+}
---
+++ Assets/Scripts/Character/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private enum State
+    {
+        Move,
+        Battle
+    }
+
+    private State currentState = State.Move;    //角色状态
+    private CharacterController m_controller;   //刚体
+    private Animator m_animator;                //动画控制
+    private float moveSpeed = 3;                //移动速度
+
+    private void Awake()
+    {
+        m_animator = this.GetComponent<Animator>();
+        m_controller = this.GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        //角色状态机
+        switch (currentState)
+        {
+            case State.Move:
+                float x = Input.GetAxis("Horizontal");
+                float z = Input.GetAxis("Vertical");
+                //向量整合要规范化（向量长度转化为1），以防斜走时速度大于正常速度
+                Vector3 nextDir = new Vector3(x, 0, z).normalized;
+                //移动
+                m_controller.Move(nextDir * moveSpeed * Time.deltaTime);
+                //移动动画
+                m_animator.SetFloat("move", Mathf.Abs(x) + Mathf.Abs(z));
+                //转向
+                transform.forward = Vector3.Lerp(transform.forward, nextDir, .3f);
+                break;
+            case State.Battle:
+                break;
+        }
+    }
+}
